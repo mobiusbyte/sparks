f87f5b457bd06275d565ab7f322cdfd5abe9bd25
Jill San Luis <jill.devs@gmail.com>
2019-08-10 13:36:08 -0400

    Verify include scenario. Also set the default behaviour to always exclude if user did not set optional fields preference. See #3

--- test/test_create.py
+++ test/test_create.py
@@ -21,11 +21,33 @@ class TestCreate:
         self._use_case.execute(create_command)
         assert_folder(expected_folder("create_simple"), self._tmp_folder)
 
-    def test_create_exclude_optinal_file_section(self):
+    def test_create_exclude_optional_file_section(self):
         create_command = CreateCommand(
             template=template_folder("with_optional_file_sections"),
             output=self._tmp_folder,
             context={"include_optional_field": False},
+        )
+        self._use_case.execute(create_command)
+        assert_folder(
+            expected_folder("create_exclude_optional_file_sections"), self._tmp_folder
+        )
+
+    def test_create_include_optional_file_section(self):
+        create_command = CreateCommand(
+            template=template_folder("with_optional_file_sections"),
+            output=self._tmp_folder,
+            context={"include_optional_field": True},
+        )
+        self._use_case.execute(create_command)
+        assert_folder(
+            expected_folder("create_include_optional_file_sections"), self._tmp_folder
+        )
+
+    def test_create_with_optional_file_section_defaults_exclude(self):
+        create_command = CreateCommand(
+            template=template_folder("with_optional_file_sections"),
+            output=self._tmp_folder,
+            context={},
         )
         self._use_case.execute(create_command)
         assert_folder(

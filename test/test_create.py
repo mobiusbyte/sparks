d40c3134d20748a7c90cde76ab32bfcad8d7d5ed
Jill San Luis <jill.devs@gmail.com>
2019-08-10 09:51:43 -0400

    Stub create tests. See #1

---
+++ test/test_create.py
@@ -0,0 +1,30 @@
+def test_create():
+    pass
+
+
+def test_create_on_non_empty_directory_should_fail():
+    pass
+
+
+def test_create_exclude_optional_content():
+    pass
+
+
+def test_create_include_optional_content():
+    pass
+
+
+def test_create_exclude_optional_directory():
+    pass
+
+
+def test_create_include_optional_directory():
+    pass
+
+
+def test_create_exclude_optional_file():
+    pass
+
+
+def test_create_include_optional_file():
+    pass
